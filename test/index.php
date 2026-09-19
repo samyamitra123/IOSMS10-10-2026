c616fe646610dd635e77af0c282fdd18cc9a508b
samyamitra123 <52238888+samyamitra123@users.noreply.github.com>
2026-09-19 11:57:16 +0530

    Updated test index

--- test/index.php
+++ test/index.php
@@ -1,4 +1,4 @@
 <?php
-echo "The site will be available soon.";
-echo "Good things take time, and we are working hard to bring you an amazing experience.";
+echo "The site will be available soon."
 ?>
+
